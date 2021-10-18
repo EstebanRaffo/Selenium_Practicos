179eb915908c4a88340ae10f941577341e093029
Esteban Raffo <e.fraffo@gmail.com>
2021-10-18 20:04:04 -0300

    18/10

--- APIsRestassured/src/test/java/Practico5/CatFact.java
+++ APIsRestassured/src/test/java/Practico5/CatFact.java
@@ -1,6 +1,11 @@
 package Practico5;
 
+import com.google.gson.Gson;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -18,14 +23,50 @@ public class CatFact {
 
     @Test
     public void getTextsTest(){
-        List<String> response =
-                given().header("Content-Type", "application/json")
-                .when().get("facts")
-                .then().assertThat().statusCode(503)
-                .extract().path("text");
+//        List<String> response =
+//                given().header("Content-Type", "application/json")
+//                .when().get("facts")
+//                .then().assertThat().statusCode(503)
+//                .extract().path("text");
+//
+//        for (String text : response){
+//            System.out.println(text);
+//        }
+    }
 
-        for (String text : response){
-            System.out.println(text);
-        }
+
+    public static Response doGetRequest(String endpoint) {
+        return
+                given().headers("Content-Type", ContentType.JSON, "Accept", ContentType.JSON)
+                .when().get(endpoint).
+                then().contentType(ContentType.JSON).log().all().extract().response();
+    }
+
+    @Test
+    public static void getCurrencyListTest(){
+        RestAssured.baseURI = "https://api.n.exchange";
+        String endpoint = "/en/api/v1/currency/";
+
+        Response response = doGetRequest(endpoint);
+        System.out.println(response.body().asString());
+        Assert.assertEquals(200, response.statusCode());
+    }
+
+    @Test
+    public static void getJsonUsersTest(){
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+        String endpoint = "/users";
+
+        Response response = doGetRequest(endpoint);
+        Assert.assertEquals(200, response.statusCode());
+
+        System.out.println(response.body().jsonPath());
+        System.out.println(response.body().getClass());
+
+        String jsonResponse = new Gson().toJson(response);
+        jsonResponse = "{\"users\": "+ jsonResponse +"}";
+        JsonPath js = new JsonPath(jsonResponse);
+        System.out.println("Se obtuvieron " + js.getInt("users.size()") + " usuarios");
+
     }
 }
---
+++ APIsRestassured/src/test/java/Practico5/entidades/Users.java
@@ -0,0 +1,15 @@
+package Practico5.entidades;
+
+public class Users {
+
+    String name;
+    String username;
+    String email;
+
+    public Users(String name, String username, String email){
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+
+}
