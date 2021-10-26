a3b40b00a5835169bc0692c7d77aa86a56d0c391
Esteban Raffo <e.fraffo@gmail.com>
2021-10-26 20:04:54 -0300

    26/10

--- APIsRestassured/src/test/java/Practico6/SalesforceRequests.java
+++ APIsRestassured/src/test/java/Practico6/SalesforceRequests.java
@@ -1,5 +1,6 @@
 package Practico6;
 
+import Clase6.Contact;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
@@ -24,7 +25,7 @@ public class SalesforceRequests {
                     .queryParam("client_id", "3MVG9p1Q1BCe9GmBFxFv86hAuBdOHjVb8SJIhkKHfVZUqdeSKAWcYNzzoLMN3v6QC0yp61kUYE1fCmekeCX.O")
                     .queryParam("client_secret", "4381C0ECBB5F8BA367E97A1B75874BCBBB3EAE68ECD34D2523740B9EC56B68C8")
                     .queryParam("username", "e.fraffo@gmail.com")
-                    .queryParam("password", "holamundo123sJkHjd5X8zNbjH46JyWllDnD")
+                    .queryParam("password", "revolucionario87Ay7nOjpbwyCVhOpVNp2pSwfTx")
                 .when()
                     .post("token")
                 .then().assertThat().statusCode(200)
@@ -35,7 +36,45 @@ public class SalesforceRequests {
         JsonPath js = new JsonPath(respuesta);
 
         String accessToken = js.get("access_token");
+        String instanceUrl = js.get("instance_url");
+        String instanceId = js.get("id");
 
         System.out.println("Access token: --> " + accessToken);
+        System.out.println("Instance Url: --> " + instanceUrl);
+        System.out.println("Instance id: --> " + instanceId);
+
+        //crear un nuevo contacto....
+        Contact contacto1 = new Contact("Pedro Gomez");
+
+        RestAssured.baseURI = instanceUrl;
+
+        String nuevoContacto = given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + accessToken)
+                .body(contacto1)
+                .when()
+                .post("/services/data/v51.0/sobjects/Contact")
+                .then()
+                .assertThat().statusCode(201).extract().asString();
+
+        System.out.println(nuevoContacto);
+
+        js = new JsonPath(nuevoContacto);
+
+        String contactId = js.get("id");
+
+        //obtener la informacion del contacto previamente creado....
+        String contactInfo =
+                given()
+                        .header("Content-Type", "application/json")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .when()
+//                    El endpoint sale de workbench
+                        .get("services/data/v51.0/sobjects/Contact/" + contactId )
+                        .then()
+                        .assertThat().statusCode(200).extract().asString();
+
+        System.out.println("***********  Contact Information ***************");
+        System.out.println("---> " + contactInfo);
     }
 }
