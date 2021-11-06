d058b2212b4a4f895d6b224d85f2917a886da26f
Esteban Raffo <e.fraffo@gmail.com>
2021-11-06 18:04:54 -0300

    6/11

--- APIsRestassured/src/test/java/Practico8/AccountsTests.java
+++ APIsRestassured/src/test/java/Practico8/AccountsTests.java
@@ -8,6 +8,8 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+
 import static io.restassured.RestAssured.given;
 
 public class AccountsTests {
@@ -82,7 +84,6 @@ public class AccountsTests {
         Assert.assertTrue(success);
     }
 
-
     @Test
     public void getAccountTest() {
         String accountInfo =
@@ -90,7 +91,7 @@ public class AccountsTests {
                     .header("Content-Type", "application/json")
                     .header("Authorization", "Bearer " + ACCESS_TOKEN)
                 .when()
-                    .get("services/data/v51.0/sobjects/Account/" + "0035f000004csvZAAQ" )
+                    .get("services/data/v51.0/sobjects/Account/" + "0015f00000BOZT6AAP" )
                 .then()
                     .log().all().assertThat().statusCode(200).extract().asString();
 
@@ -99,7 +100,7 @@ public class AccountsTests {
 
         JsonPath js = new JsonPath(accountInfo);
         String accountName = js.get("Name");
-        Assert.assertEquals(accountName, "United Oil & Gas Corp.", "Error: se esperaba otro nombre de cuenta!!");
+        Assert.assertEquals(accountName, "Maria Ramirez", "Error: se esperaba otro nombre de cuenta!!");
     }
 
     @Test
@@ -109,12 +110,151 @@ public class AccountsTests {
                     .header("Content-Type", "application/json")
                     .header("Authorization", "Bearer " + ACCESS_TOKEN)
                 .when()
-                    .delete("services/data/v51.0/sobjects/Contact/" + "0035f000004csvZAAQ" )
+                    .delete("services/data/v51.0/sobjects/Account/" + "0015f000007w2bUAAQ" )
                 .then()
                     .assertThat().statusCode(204).extract().asString();
 
         System.out.println("***********  Contact Information ***************");
         System.out.println("---> " + responseInfo);
+    }
+
+    /*
+     @Esteban
+     02-11-2021
+     Se realiza la prueba de un POST sin body
+     Se espera mensajes de error
+    */
+    @Test
+    public void emptyBodyErrorTest() {
+
+        String newAccountResponse =
+                given()
+                    .header("Content-type", "application/json")
+                    .header("Authorization", "Bearer " + ACCESS_TOKEN)
+                    //.body(anAccount)
+                .when()
+                    .post("/services/data/v51.0/sobjects/Account")
+                .then()
+                    .log().all().assertThat().statusCode(400).extract().asString();
+
+        System.out.println("Response as String --> " + newAccountResponse);
+
+        Assert.assertTrue(newAccountResponse.contains("JSON_PARSER_ERROR"), "Error: deberia estar el error code JSON_PARSER_ERROR");
+        Assert.assertTrue(newAccountResponse.contains("The HTTP entity body is required, but this request has no entity body."), "Error: deberia estar el mensaje no body");
+
+        Response responseNewAccount =
+                given()
+                    .header("Content-type", "application/json")
+                    .header("Authorization", "Bearer " + ACCESS_TOKEN)
+                    //.body(anAccount)
+                .when()
+                    .post("/services/data/v51.0/sobjects/Account")
+                .then()
+                    .log().all().assertThat().statusCode(400).extract().response();
+
+        JsonPath jsonPathEvaluator = responseNewAccount.jsonPath();
+        String message = jsonPathEvaluator.get("[0].message");
+        System.out.println("message obtenido: " + message);
+
+        Assert.assertEquals(message, "The HTTP entity body is required, but this request has no entity body.", "No se obtuvo el mensaje esperado");
+    }
+
+
+    /*
+     @Esteban
+     06-11-2021
+     Se realiza la prueba de un POST con json mal formado
+     Se espera mensajes de error
+    */
+    @Test
+    public void invalidJsonTest() {
+
+        String newAccount = "{\n" +
+                            "    \"Name\": \"Marta Perez\",\n" +
+                            "    \"Description\" : \"Hi, this is Marta account!!\",\n" +
+                            "    \"Site\" \"marta.com.ar\"\n" +
+                            "}";
+
+        String newAccountResponse =
+                given()
+                    .header("Content-type", "application/json")
+                    .header("Authorization", "Bearer " + ACCESS_TOKEN)
+                    .body(newAccount)
+                .when()
+                    .post("/services/data/v51.0/sobjects/Account")
+                .then()
+                    .log().all().assertThat().statusCode(400).extract().asString();
+
+        System.out.println("Response as String --> " + newAccountResponse);
+
+        Assert.assertTrue(newAccountResponse.contains("JSON_PARSER_ERROR"), "Error: deberia estar el error code JSON_PARSER_ERROR");
+        Assert.assertTrue(newAccountResponse.contains("code 34"), "Error: deberia estar el error code 34");
+
+        Response responseNewAccount =
+                given()
+                    .header("Content-type", "application/json")
+                    .header("Authorization", "Bearer " + ACCESS_TOKEN)
+                    .body(newAccount)
+                .when()
+                    .post("/services/data/v51.0/sobjects/Account")
+                .then()
+                    .log().all().assertThat().statusCode(400).extract().response();
+
+        JsonPath jsonPathEvaluator = responseNewAccount.jsonPath();
+        String message = jsonPathEvaluator.get("[0].message");
+        System.out.println("message obtenido: " + message);
+        String errorCode = jsonPathEvaluator.get("[0].errorCode");
+
+        Assert.assertTrue(errorCode.contains("JSON_PARSER_ERROR"), "Error: deberia estar el error code JSON_PARSER_ERROR");
+        Assert.assertTrue(message.contains("code 34"), "No se obtuvo el error code 34");
+    }
+
+
+    /*
+     @Esteban
+     06-11-2021
+     Se realiza la prueba de un POST con json invalido
+     Se espera mensajes de error
+    */
+    @Test
+    public void invalidJsonXpTest() {
+
+        String newAccount = "{\n" +
+                            "    \"Name Marta Perez\"\n" +
+                            "}";
+
+        String newAccountResponse =
+                given()
+                    .header("Content-type", "application/json")
+                    .header("Authorization", "Bearer " + ACCESS_TOKEN)
+                    .body(newAccount)
+                .when()
+                    .post("/services/data/v51.0/sobjects/Account")
+                .then()
+                    .log().all().assertThat().statusCode(400).extract().asString();
+
+        System.out.println("Response as String --> " + newAccountResponse);
+
+        Assert.assertTrue(newAccountResponse.contains("JSON_PARSER_ERROR"), "Error: deberia estar el error code JSON_PARSER_ERROR");
+        Assert.assertTrue(newAccountResponse.contains("code 125"), "Error: deberia estar el error code 125");
+
+        Response responseNewAccount =
+                given()
+                    .header("Content-type", "application/json")
+                    .header("Authorization", "Bearer " + ACCESS_TOKEN)
+                    .body(newAccount)
+                .when()
+                    .post("/services/data/v51.0/sobjects/Account")
+                .then()
+                    .log().all().assertThat().statusCode(400).extract().response();
+
+        JsonPath jsonPathEvaluator = responseNewAccount.jsonPath();
+        String message = jsonPathEvaluator.get("[0].message");
+        System.out.println("message obtenido: " + message);
+        String errorCode = jsonPathEvaluator.get("[0].errorCode");
+
+        Assert.assertTrue(errorCode.contains("JSON_PARSER_ERROR"), "Error: deberia estar el error code JSON_PARSER_ERROR");
+        Assert.assertTrue(message.contains("code 125"), "No se obtuvo el error code 125");
     }
 
 
@@ -127,7 +267,7 @@ public class AccountsTests {
     @Test
     public void emptyTokenErrorTest() {
 
-        Account anAccount = new Account("Juan", "Mi cuenta", "juan@mail.com");
+        Account anAccount = new Account("Juan", "Mi cuenta", "juan.com.ar");
 
         String newAccountResponse =
                 given()
@@ -137,13 +277,15 @@ public class AccountsTests {
                 .when()
                     .post("/services/data/v51.0/sobjects/Account")
                 .then()
-                    .log().all().assertThat().statusCode(401).extract().asString();
+                    .log().all().assertThat().statusCode(401)
+//                    .body("[0].message", containsString("INVALID_HEADER_TYPE"))
+//                    .body("[0].message", containsString("INVALID_AUTH_HEADER"))
+                    .extract().asString();
 
         System.out.println("--> " + newAccountResponse);
 
         Assert.assertTrue(newAccountResponse.contains("INVALID_SESSION_ID"), "Error: deberia estar el error code invalid session id");
         Assert.assertTrue(newAccountResponse.contains("Session expired or invalid"), "Error: deberia estar el mensaje session expired");
-
 
         Response response =
                 given()
@@ -166,28 +308,4 @@ public class AccountsTests {
         Assert.assertEquals(errorCode, "INVALID_SESSION_ID", "Error: se esperaba otro session id");
     }
 
-    /*
-     @Esteban
-     02-11-2021
-     Se realiza la prueba de un POST sin body
-     Se espera mensajes de error
-    */
-    @Test
-    public void emptyBodyErrorTest() {
-
-        String newAccountResponse =
-                given()
-                    .header("Content-type", "application/json")
-                    .header("Authorization", "Bearer " + ACCESS_TOKEN)
-                    //.body(anAccount)
-                .when()
-                    .post("/services/data/v51.0/sobjects/Account")
-                .then()
-                    .log().all().assertThat().statusCode(400).extract().asString();
-
-        System.out.println("--> " + newAccountResponse);
-
-        Assert.assertTrue(newAccountResponse.contains("JSON_PARSER_ERROR"), "Error: deberia estar el error code JSON_PARSER_ERROR");
-        Assert.assertTrue(newAccountResponse.contains("The HTTP entity body is required, but this request has no entity body."), "Error: deberia estar el mensaje no body");
-    }
 }
